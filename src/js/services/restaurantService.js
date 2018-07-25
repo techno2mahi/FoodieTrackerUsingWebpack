5fb07bbda60379ef8e119ce713f25648fe8988e1
techno2mahi <techno2mahi@gmail.com>
2018-07-25 15:07:00 +0530

    Minor lint fixes

--- src/js/services/restaurantService.js
+++ src/js/services/restaurantService.js
@@ -1,34 +1,26 @@
-import { Constants } from "../shared/constants";
-import { DataService } from "./dataService";
-export class RestaurantService {
+import { Constants } from '../shared/constants';
+import { DataService } from './dataService';
+
+class RestaurantService {
   constructor() {
     this.dataService = new DataService(Constants.ZOMATO_AUTH_KEY);
   }
 
   searchRestaurants(query, offset) {
     const cityId = 4;
-    this.dataService.fetchOptions.method = "GET";
-    let queryParams = `?entity_id=${cityId}&entity_type=city&q=${encodeURI(
-      query
+    this.dataService.fetchOptions.method = 'GET';
+    const queryParams = `?entity_id=${cityId}&entity_type=city&q=${encodeURI(
+      query,
     )}&count=${Constants.PAGING_COUNT}&start=${offset}`;
     let searchUrl = Constants.RESTAURANTS_SEARCH_URL;
-    searchUrl = searchUrl + queryParams;
+    searchUrl += queryParams;
     return this.dataService.getJSON(searchUrl);
   }
 
-  postJSON(url) {
-    this.fetchOptions.method = "POST";
-    var dataPromise = fetch(url, this.fetchOptions);
-    return new Promise((resolve, reject) => {
-      dataPromise
-        .then(res => {
-          res.json().then(data => {
-            resolve(data);
-          });
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
+  getRestaurantDetail(id) {
+    const url = `${Constants.ZOMATO_API_BASE}restaurant?res_id=${id}`;
+    return this.dataService.getJSON(url);
   }
 }
+
+export default RestaurantService;
--- src/js/shared/constants.js
+++ src/js/shared/constants.js
@@ -1,33 +1,41 @@
-export class Constants {
+class Constants {
   static get ZOMATO_API_BASE() {
-    return "https://developers.zomato.com/api/v2.1/";
+    return 'https://developers.zomato.com/api/v2.1/';
   }
 
   static get COLLECTIONS_API_BASE() {
-    return "http://localhost:3004/";
+    return 'http://localhost:3004/';
   }
 
   static get COLLECTIONS_API() {
-    return this.COLLECTIONS_API_BASE + "collections";
+    return `${this.COLLECTIONS_API_BASE}collections`;
   }
 
   static get ADD_COLLECTIONS_URL() {
-    return this.COLLECTIONS_API_BASE + "collections";
+    return `${this.COLLECTIONS_API_BASE}collections`;
   }
 
   static get RESTAURANTS_SEARCH_URL() {
-    return this.ZOMATO_API_BASE + "search";
+    return `${this.ZOMATO_API_BASE}search`;
   }
 
   static get ZOMATO_DEFAULT_IMAGE() {
-    return "https://b.zmtcdn.com/data/pictures/6/18011656/95177ed226010a6be13935df30d552e4.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A";
+    return 'https://b.zmtcdn.com/data/pictures/6/18011656/95177ed226010a6be13935df30d552e4.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A';
   }
 
   static get ZOMATO_AUTH_KEY() {
-    return "d9ccd37d2d650e0f24917ab3ea4126be";
+    return 'd9ccd37d2d650e0f24917ab3ea4126be';
   }
 
   static get PAGING_COUNT() {
     return 8;
   }
 }
+
+const ReduxConstants = {
+  ADD_RESTAURANTS: 'ADD_RESTAURANTS',
+  DETAIL_RESTAURANT: 'DETAIL_RESTAURANT',
+  CURRENT_VIEW: 'CURRENT_VIEW',
+};
+
+export { ReduxConstants, Constants };
